0f476e4ebb89dec0015c9fe4bd89fc56439dae7f
John Halazonetis <109440809+johnhalz@users.noreply.github.com>
2024-07-05 18:17:59 +0200

    Added configs and script to download

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# orcaconfig
-Configuration for OrcaSlicer
+# OrcaSlicer Config
+
+Configuration for OrcaSlicer (note that this guide is only for macOS).
+
+## Download OrcaSlicer
+You can download [OrcaSlicer](https://github.com/SoftFever/OrcaSlicer) using homebrew:
+
+``` bash
+brew install orcaslicer
+```
+
+Alternatively, you can go and download the [latest release on their GitHub repo](https://github.com/SoftFever/OrcaSlicer/releases).
+
+## Using this Config
+
+This repo contains configurations for filament, machines and processes. You can download these configs to your machine using the following command:
+
+``` bash
+curl -s https://raw.githubusercontent.com/johnhalz/orcaconfig/main/download_configs.sh | bash
+```
---
+++ download_configs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Define the GitHub repository details
+GITHUB_USERNAME="johnhalz"
+GITHUB_REPO="orcaconfig"
+GITHUB_BRANCH="main"
+
+# Define the local destination paths
+LOCAL_BASE_PATH="$HOME/Library/Application Support/OrcaSlicer/user/default"
+FILAMENT_PATH="$LOCAL_BASE_PATH/filament"
+MACHINE_PATH="$LOCAL_BASE_PATH/machine"
+PROCESS_PATH="$LOCAL_BASE_PATH/process"
+
+# Create directories if they do not exist
+mkdir -p "$FILAMENT_PATH" "$MACHINE_PATH" "$PROCESS_PATH"
+
+# Download files from GitHub and save them to the local paths
+download_files() {
+    local folder=$1
+    local dest_path=$2
+    curl -s "https://api.github.com/repos/$GITHUB_USERNAME/$GITHUB_REPO/contents/$folder?ref=$GITHUB_BRANCH" |
+    jq -r '.[] | select(.type == "file") | .download_url' |
+    while read -r url; do
+        file_name=$(basename "$url")
+        curl -s "$url" -o "$dest_path/$file_name"
+    done
+}
+
+# Download files for each folder
+download_files "filament" "$FILAMENT_PATH"
+download_files "machine" "$MACHINE_PATH"
+download_files "process" "$PROCESS_PATH"
+
+echo "Files downloaded successfully."
